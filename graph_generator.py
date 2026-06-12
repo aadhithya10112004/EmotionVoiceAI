0781af52256f11955b405ad19e5d4e0b87df3213
Aadhithya S <aadhithya.s2023ai-ds@sece.ac.in>
2026-06-12 21:36:02 +0530

    Fixed outputs folder creation

--- graph_generator.py
+++ graph_generator.py
@@ -1,27 +1,53 @@
+import os
 import matplotlib.pyplot as plt
 
 
 def create_emotion_chart(emotions):
 
+    # Create outputs folder if it doesn't exist
+    os.makedirs(
+        "outputs",
+        exist_ok=True
+    )
+
     labels = []
     scores = []
 
     for item in emotions:
-        labels.append(item["label"].upper())
-        scores.append(item["score"] * 100)
 
-    plt.figure(figsize=(8, 5))
+        labels.append(
+            item["label"].upper()
+        )
 
-    plt.bar(labels, scores)
+        scores.append(
+            item["score"] * 100
+        )
 
-    plt.title("Emotion Distribution")
+    plt.figure(
+        figsize=(8, 5)
+    )
 
-    plt.xlabel("Emotion")
+    plt.bar(
+        labels,
+        scores
+    )
 
-    plt.ylabel("Confidence (%)")
+    plt.title(
+        "Emotion Distribution"
+    )
+
+    plt.xlabel(
+        "Emotion"
+    )
+
+    plt.ylabel(
+        "Confidence (%)"
+    )
 
     plt.tight_layout()
 
-    plt.savefig("outputs/emotion_chart.png")
+    plt.savefig(
+        "outputs/emotion_chart.png"
+    )
 
     plt.close()
